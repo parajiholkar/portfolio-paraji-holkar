9e451af8af169d2a4fed68d2a3c4cbe05d7fdd88
Parajiholkar <parajiholkar81@gmail.com>
2023-09-17 22:33:58 +0530

    "responsive"

--- logic.js
+++ logic.js
@@ -1,17 +1,45 @@
-  
-document.getElementById('Education').addEventListener('click',(e)=>{
-    e.preventDefault();
-    this.document.getElementById('About').getAttribute('hidden') = true ;
-    this.document.getElementsByClassName('education').getAttribute('hidden') = false ;
+const tabs = document.querySelectorAll('.tab-btn');
+const tab_content = document.querySelectorAll('.content');
+const sections = document.querySelectorAll('section');
+const navbar_links = document.querySelectorAll('header nav a');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+tabs.forEach((tab,index)=>{
+    tab.addEventListener('click',(e)=>{
+        tabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
+
+        var line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + 'px';
+        line.style.left = e.target.offsetLeft + 'px';
+
+        tab_content.forEach(content=>{content.classList.remove('active')});
+        tab_content[index].classList.add('active');
+
+    });
+
 });
-document.getElementById('Project').addEventListener('click',(e)=>{
-    e.preventDefault();
-    this.document.getElementById('About').getAttribute('hidden') = true ;
-    this.document.getElementsByClassName('project').getAttribute('hidden') = false ;
-});
-document.getElementById('Contact').addEventListener('click',(e)=>{
-    e.preventDefault();
-    this.document.getElementById('About').getAttribute('hidden') = true ;
-    this.document.getElementsByClassName('contact').getAttribute('hidden') = false ;
-});
+
+window.onscroll = ()=>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight ;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height){
+            navbar_links.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
 
